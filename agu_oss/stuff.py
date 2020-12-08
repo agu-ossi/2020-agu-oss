ba6359d9758db8ad5f70a4f75302f06188e6508e
banesullivan <bane.sullivan@kitware.com>
2020-12-08 10:47:30 -0700

    Add GitHub action to build sphinx documentation

---
+++ agu_oss/stuff.py
@@ -0,0 +1,20 @@
+"""A sample module."""
+
+
+def divide(a, b):
+    """Divide two numbers.
+
+    Parameters
+    ----------
+    a : int, float
+        The dividend
+
+    b : int, float
+        The divisor
+
+    Return
+    ------
+    int, float: the quotient
+
+    """
+    return a / b
